59f91d30f9a180b1aad2cd73de1f31c6b8014a8c
DawidGajownik <dawidgajownik6@gmail.com>
2024-08-12 19:27:45 +0200

    Kod Factors.java

--- ModulySpecjalistyczne/src/main/java/pl/coderslab/tydzien5/z1tdd/Factors.java
+++ ModulySpecjalistyczne/src/main/java/pl/coderslab/tydzien5/z1tdd/Factors.java
@@ -1,9 +1,16 @@
 package pl.coderslab.tydzien5.z1tdd;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Factors {
     static List<Integer> generatePrimeFactors(int n) {
-        return null;
+        List<Integer>list = new ArrayList<>();
+        for (int i = 1; i <=n ; i++) {
+            if (n%i==0) {
+                list.add(i);
+            }
+        }
+        return list;
     }
 }
